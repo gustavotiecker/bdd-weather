b11038ecdc5e6aa16c970126e05f2452a44e36b2
Gustavo Tiecker <gustavotiecker@gmail.com>
2021-05-19 18:24:27 -0300

    Initial Commit

---
+++ BDD-Weather/BDD_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  BDD_WeatherApp.swift
+//  BDD-Weather
+//
+//  Created by Gustavo Tiecker on 19/05/21.
+//
+
+import SwiftUI
+
+@main
+struct BDD_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BDD-Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BDD-Weather
+//
+//  Created by Gustavo Tiecker on 19/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
